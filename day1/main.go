e07eab3d5c5563d1483b00b4510a20406ea0956c
Nathaël Arki <nathael.arki@golem.ai>
2020-12-07 22:04:30 +0100

    Clean day 1.

--- day1/main.go
+++ day1/main.go
@@ -17,16 +17,37 @@ func main() {
 	byteValue, _ := ioutil.ReadAll(textFile)
 	values := strings.Split(string(byteValue), "\n")
 
-	for _, v := range values {
-		base, _ := strconv.Atoi(v)
-		for _, v := range values {
-			add1, _ := strconv.Atoi(v)
-			for _, v := range values {
-				add2, _ := strconv.Atoi(v)
-				if base+add1+add2 == 2020 {
-					fmt.Println(base * add1 * add2)
+	numbers := make([]int, len(values))
+	for i, v := range values {
+		number, _ := strconv.Atoi(v)
+		numbers[i] = number
+	}
+
+	fmt.Println(getSumProduct(2020, numbers, 2))
+	fmt.Println(getSumProduct(2020, numbers, 3))
+}
+
+// 2 first implem => 40 200
+// 2 second case => 20 300
+// 3 first implem => 8 040 200
+// 3 second case => 1 373 700
+func getSumProduct(target int, numbers []int, factorsNumber int) int {
+	for i, n1 := range numbers {
+		for j := i; j < len(numbers); j++ {
+			n2 := numbers[j]
+			if factorsNumber == 2 && n1+n2 == 2020 {
+				return n1 * n2
+			}
+			if factorsNumber >= 3 {
+				for k := j; k < len(numbers); k++ {
+					n3 := numbers[k]
+					if factorsNumber == 3 && n1+n2+n3 == 2020 {
+						return n1 * n2 * n3
+					}
 				}
 			}
 		}
 	}
+
+	return -1
 }
