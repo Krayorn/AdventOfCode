c51d5baad36433da27606b37394e3e9d00217f73
Nathaël Arki <nathael.arki@golem.ai>
2020-12-08 21:40:11 +0100

    Clean day 3.

--- day3/main.go
+++ day3/main.go
@@ -16,10 +16,7 @@ func main() {
 	byteValue, _ := ioutil.ReadAll(textFile)
 	values := strings.Split(string(byteValue), "\n")
 
-	maxRight := len(values[0])
-
-	res := 1
-	options := [][]int{
+	params := [][]int{
 		[]int{1, 1},
 		[]int{3, 1},
 		[]int{5, 1},
@@ -27,7 +24,10 @@ func main() {
 		[]int{1, 2},
 	}
 
-	for _, v := range options {
+	maxRight := len(values[0])
+	res := make([]int, len(params))
+
+	for paramID, v := range params {
 		total := 0
 		right, down := v[0], v[1]
 		for i, j := 0, 0; i < len(values); i, j = i+down, j+right {
@@ -35,7 +35,14 @@ func main() {
 				total++
 			}
 		}
-		res = res * total
+		res[paramID] = total
 	}
-	fmt.Println(res)
+
+	total2 := 1
+	for _, v := range res {
+		total2 *= v
+	}
+
+	fmt.Println("The number of trees encoutered for the slope right 3, down 1 is =>", res[1])
+	fmt.Println("The product of all the trees encoutered for each specified parameters is =>", total2)
 }
