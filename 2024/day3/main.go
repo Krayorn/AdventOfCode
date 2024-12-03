11bd6bf47c8dd77f5cd1bbb50416f4b79d3e4729
Krayorn <nath.arki36@gmail.com>
2024-12-03 10:01:08 +0100

    day3

---
+++ 2024/day3/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+//go:embed input.txt
+var content string
+
+func main() {
+	r, _ := regexp.Compile("mul\\((\\d+),(\\d+)\\)|don't\\(\\)|do\\(\\)")
+
+	matches := r.FindAllStringSubmatch(content, -1)
+
+	sum := 0
+	enabled := true
+	for _, match := range matches {
+		fmt.Println(match, enabled)
+		if match[0] == "don't()" {
+			enabled = false
+			continue
+		}
+
+		if match[0] == "do()" {
+			enabled = true
+			continue
+		}
+
+		if !enabled {
+			continue
+		}
+
+		n1, _ := strconv.Atoi(match[1])
+		n2, _ := strconv.Atoi(match[2])
+		fmt.Println(match, n1, n2)
+		sum += n1 * n2
+	}
+
+	fmt.Println(sum)
+}
